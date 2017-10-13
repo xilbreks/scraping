c9b7436eb2791b50fa269cd97a2b11e8d04bc93d
Socks <Xil.Breks@gmail.com>
2017-10-12 22:46:37 -0500

    scaneo de paginas web

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var cheerio = require('cheerio');
 var robados = [];
 var url = 'http://www.unap.edu.pe/cidiomas/index.php?i=0&z=1&pag=recordar_pas&m=1';
 const desde = 140000;
-const hasta = 150000;
+const hasta = 141000;
 
 var consultasRestantes = hasta-desde+1;
 
@@ -52,7 +52,7 @@ function comprobarFinalizacion(){
         if(b.codigo>a.codigo) return -1;
         return 0;
     })
-    fs.writeFile('codigos.json', JSON.stringify(robados), function(err){
+    fs.writeFile('codigos0m.json', JSON.stringify(robados), function(err){
       console.log('Archivo escrito correctamente! - verifique el archivo codigos.json');
     })
   }else{
